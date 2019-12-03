7f4d41e1f1e16e5e1e44e325e524a113baa03a36
CHAI53 <skymise@gmail.com>
2019-12-03 17:18:29 +0900

    수정중

--- src/Views/Signup/SignupView.js
+++ src/Views/Signup/SignupView.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { Row, Col, Card, Form, Input, Button } from "antd";
+import "./index.less";
+import { isFulfilled } from "q";
 
 export default class SignupView extends Component {
   render() {
     return (
       <div>
-        SignupView
-        <div className="App">
-          <Button type="primary">Button</Button>
-        </div>
+        <Row type="flex" justify="center" align="middle">
+          <Col span={16} style={{ width: "600px" }}>
+            <Card className="signupCard">
+              <Form onSubmit={this.handleSubmit} className="signupForm">
+                <Form.Item className="formInput">
+                  <Input placeholder="이메일" />
+                </Form.Item>
+                <Form.Item className="formInput">
+                  <Input.Password placeholder="비밀번호" />
+                </Form.Item>
+                <Form.Item className="formInput">
+                  <Input.Password placeholder="비밀번호 확인" />
+                </Form.Item>
+                <Form.Item className="formInput">
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                  >
+                    가입하기
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
